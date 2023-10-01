861532376a20cca7776bd4e60075629cfaa7d77b
Rupert Caingal <70898073+RupertC07@users.noreply.github.com>
2023-10-01 09:39:33 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import { BrowserRouter, HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter>
-      {" "}
+    <HashRouter basename = {'/Lyric-and-thoughts/'}>
+
       <App />
     </HashRouter>
   </React.StrictMode>
