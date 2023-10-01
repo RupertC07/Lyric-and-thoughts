4153d6340efc849a28760bb26a0332c9c249d981
Rupert Caingal <70898073+RupertC07@users.noreply.github.com>
2023-10-01 10:56:02 +0800

    Update 404.jsx

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,7 +1,7 @@
 export const NotFound = () => {
   return (
     <div className="flex justify-center items-center">
-      <h2 className="text-xl text-center text-slate">404 NOT FOUND</h2>
+      <h2 className="text-xl text-center text-slate">Lyric and Thougts</h2>
     </div>
   );
 };
